99ea4358b68882941779cb206d3fe7565c5297d8
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-04-10 13:43:07 -0400

    feat
    to do list using jquery

---
+++ script.js
@@ -0,0 +1,26 @@
+$(.input).each(function(i) {
+    
+    let newElement = $('<li class ="inputValue>inputValue</li> ');
+    $('body').append(newElement);
+
+    if(inputValue().length < 1) {
+        alert('You must write something!');
+        else {
+            $('#list').append(li);
+        }
+    }
+
+    $('input').on('dblclick', function (event) {
+    'crossOut'
+    });
+
+    let newElement = $('crossOutButton');
+    $('.crossOutButton').append(document.CreateTextNode(X));
+    $('.li').append('.crossOutButton');
+
+    li.on('click', function() {
+        li.addClass('delete');
+    });
+
+    $('#list').sortable();
+});
